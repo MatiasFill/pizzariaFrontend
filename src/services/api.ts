2ab41d4eb9f804f404e8f94870689e722310ff3f
MatiasFill <rmscontato@gmail.com>
2025-01-02 23:14:00 -0300

    corrigindo o axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,5 @@
 import axios from 'axios'
 export const api = axios.create({
-    baseURL: process.env.API_BASE_URL || 'http://localhost:3000',
+    baseURL: process.env.NEXT_PUBLIC_API || 'http://localhost:3000',
     timeout: 10000,
-  });
-  
-
-
-
-/*export const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API
-}) */
+})
