06774d99e02e109c89abd5ed0d425a2f967d37e5
MatiasFill <rmscontato@gmail.com>
2025-01-02 22:55:48 -0300

    reajustando o axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,12 @@
 import axios from 'axios'
-export const api = axios.create({
+const api = axios.create({
+    baseURL: process.env.API_BASE_URL || 'http://localhost:3000',
+    timeout: 10000,
+  });
+  
+
+
+
+/*export const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API
-})
+}) */
