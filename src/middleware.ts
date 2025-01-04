9c7c0fcb344a08506232e28827aa4e06716fa36a
MatiasFill <rmscontato@gmail.com>
2025-01-04 13:00:42 -0300

    Ajust middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { getCookieServer } from '@/lib/cookieServer'
 import { api } from "@/services/api"
-//import { stringify } from 'querystring';
 
 export async function middleware(req: NextRequest){
   const { pathname } = req.nextUrl
@@ -10,15 +9,14 @@ export async function middleware(req: NextRequest){
     return NextResponse.next();
   }
 
-    //const token = JSON.stringify( getCookieServer());
-    const token = await  getCookieServer();
+  const token = await getCookieServer();
   
   if(pathname.startsWith("/dashboard")){
     if(!token){
       return NextResponse.redirect(new URL("/", req.url))
     }
 
-    const isValid = await validateToken(token)
+    const isValid =  validateToken(token)
     console.log(isValid);
 
     if(!isValid){
